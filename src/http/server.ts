68e8a1093a1b1a0d3eff7df44a72085c198aa5a2
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2024-02-08 11:24:49 -0300

    :grin: added real time voting count with ws and redis

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,8 @@ import cookie from '@fastify/cookie'
 import { createPoll } from './routes/createPoll'
 import { getPoll } from './routes/getPoll'
 import { voteOnPoll } from './routes/voteOnPoll'
+import websocket from '@fastify/websocket'
+import { pollResults } from './ws/pollResults'
 
 const app = fastify()
 
@@ -11,10 +13,12 @@ app.register(cookie, {
   hook: 'onRequest',
   parseOptions: {}
 })
+app.register(websocket)
 
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({port: 3333}).then(() => {
   console.log('HTTP Server Running!')
